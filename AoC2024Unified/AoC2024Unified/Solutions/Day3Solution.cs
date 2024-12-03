70ae5f34177b7fbe59c178ad43a3a8310306db51
Rea Klairi Tasopoulou <104150585+GistOfSpirit@users.noreply.github.com>
2024-12-03 14:16:27 +0000

    Added day 3 B solution

--- AoC2024Unified/AoC2024Unified/Solutions/Day3Solution.cs
+++ AoC2024Unified/AoC2024Unified/Solutions/Day3Solution.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -10,13 +11,11 @@ namespace AoC2024Unified.Solutions
     {
         const int DayNum = 3;
 
-        private static async Task SolveA(string dayNum, bool isReal)
+        private static int SolveA(string inputString)
         {
-            string inputString = await Common.ReadFile(dayNum, isReal);
-
             string pattern = @"mul\((?<num1>[0-9]+),(?<num2>[0-9]+)\)";
 
-            var matches = Regex.Matches(inputString, pattern,
+            MatchCollection matches = Regex.Matches(inputString, pattern,
                 RegexOptions.None, TimeSpan.FromSeconds(3))
                 ?? throw new InvalidOperationException("Null object matches");
 
@@ -30,27 +29,87 @@ namespace AoC2024Unified.Solutions
                 total += num1 * num2;
             }
 
-            Console.WriteLine($"The total is {total}.");
+            return total;
         }
 
-        private static async Task SolveB(string dayNum, bool isReal)
+        private static async Task<int> SolveA(string dayNum, bool isReal)
         {
-            await Task.Delay(0);
+            string inputString = await Common.ReadFile(dayNum, isReal);
+
+            return SolveA(inputString);
+        }
+
+        private static void AddIndexes(List<(int, bool)> list,
+            string input, string instr, bool value)
+        {
+            MatchCollection matches = Regex.Matches(input, Regex.Escape(instr),
+                RegexOptions.None, TimeSpan.FromSeconds(3))
+                ?? throw new InvalidOperationException("Null object matches");
+
+            foreach (Match match in matches)
+            {
+                list.Add((match.Index, value));
+            }
+        }
+
+        private static async Task<int> SolveB(string dayNum, bool isReal)
+        {
+            const string instrOn = "do()";
+            const string instrOff = "don't()";
+
+            string inputString = await Common.ReadFile(dayNum, isReal);
+
+            var instrPositions = new List<(int, bool)>();
+
+            AddIndexes(instrPositions, inputString, instrOn, true);
+            AddIndexes(instrPositions, inputString, instrOff, false);
+
+            instrPositions.Sort((x, y) => x.Item1.CompareTo(y.Item1));
+
+            var newInputSb = new StringBuilder();
+
+            newInputSb.Append(inputString[..instrPositions[0].Item1]);
+
+            for (int i = 0; i < instrPositions.Count; ++i)
+            {
+                (int, bool) item = instrPositions[i];
+
+                if (item.Item2)
+                {
+                    int nextIndex = instrPositions.Count <= i + 1
+                        ? inputString.Length - 1
+                        : instrPositions[i + 1].Item1;
+
+                    newInputSb.Append(
+                        inputString[item.Item1..nextIndex]);
+                }
+            }
+
+            // There is a bug here: We've effectively removed the "don't()"
+            // sections. If the input text had something like
+            // "mudon't()fdsfdfdsfdsfdsffdo()l(2,3)"
+            // this would turn into mul(2,3) and do an extra multiplication.
+            // I might fix this later.
+            return SolveA(newInputSb.ToString());
         }
 
         public async Task Solve(bool isReal)
         {
+            int totalA, totalB;
+
             if (isReal)
             {
                 string dayNum = $"{DayNum}";
-                await SolveA(dayNum, isReal);
-                await SolveB(dayNum, isReal);
+                totalA = await SolveA(dayNum, isReal);
+                totalB = await SolveB(dayNum, isReal);
             }
             else
             {
-                await SolveA($"{DayNum}a", isReal);
-                await SolveB($"{DayNum}b", isReal);
+                totalA = await SolveA($"{DayNum}a", isReal);
+                totalB = await SolveB($"{DayNum}b", isReal);
             }
+
+            Console.WriteLine($"The totals are A:{totalA}, B:{totalB}.");
         }
     }
 }
