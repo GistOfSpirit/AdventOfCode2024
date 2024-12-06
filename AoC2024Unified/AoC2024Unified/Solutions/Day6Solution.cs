31c57a8808c857e1c31230224deb1dc6a058dad1
Rea Klairi Tasopoulou <104150585+GistOfSpirit@users.noreply.github.com>
2024-12-06 22:22:59 +0000

    Complete brute force of B, finally correct

--- AoC2024Unified/AoC2024Unified/Solutions/Day6Solution.cs
+++ AoC2024Unified/AoC2024Unified/Solutions/Day6Solution.cs
@@ -6,6 +6,7 @@ namespace AoC2024Unified.Solutions
     {
         private const int DayNum = 6;
 
+        private const char Empty = '.';
         private const char Obstacle = '#';
         private const char GuardNorth = '^';
         private const char GuardSouth = 'v';
@@ -99,8 +100,7 @@ namespace AoC2024Unified.Solutions
                 || loc.Y < 0 || loc.Y >= map.Length;
 
         private static AdvancementResult AdvanceGuard(string[] map,
-            List<GuardPointDir> metObstacles, ref Point loc,
-            List<Point>? retconSpots = null)
+            List<GuardPointDir> metObstacles, ref Point loc)
         {
             char guard = map[loc.Y][loc.X];
 
@@ -138,29 +138,6 @@ namespace AoC2024Unified.Solutions
             }
             else
             {
-                if (retconSpots != null)
-                {
-                    string[] mapCopy = (string[])map.Clone();
-                    Point locCopy = loc;
-                    var metObstaclesCopy
-                        = new List<GuardPointDir>(metObstacles);
-
-                    TurnGuard(mapCopy, locCopy);
-
-                    AdvancementResult result;
-
-                    do
-                    {
-                        result = AdvanceGuard(
-                            mapCopy, metObstaclesCopy, ref locCopy);
-                    } while (result == AdvancementResult.InMap);
-
-                    if (result == AdvancementResult.Revisit)
-                    {
-                        retconSpots.Add(newLoc);
-                    }
-                }
-
                 Common.UpdateMatrix(map, loc, GuardVisited);
                 Common.UpdateMatrix(map, newLoc, guard);
                 loc = newLoc;
@@ -172,21 +149,38 @@ namespace AoC2024Unified.Solutions
         private static int CountVisited(string[] map)
             => map.Sum((r) => r.Count((c) => c == GuardVisited));
 
+        private IEnumerable<string[]> AddOneObstacle(string[] map)
+        {
+            for (int row = 0; row < map.Length; ++row)
+            {
+                for (int col = 0; col < map[row].Length; ++col)
+                {
+                    if (map[row][col] == Empty)
+                    {
+                        string[] newMap = (string[])map.Clone();
+                        Common.UpdateMatrix(
+                            newMap, new Point(col, row), Obstacle);
+                        yield return newMap;
+                    }
+                }
+            }
+        }
+
         public async Task Solve(bool isReal)
         {
             string[] map = await Common.ReadFileAsMatrix(isReal, DayNum);
+            string[] mapOrig = (string[])map.Clone();
 
             Point guardLoc = LocateGuard(map);
+            Point guardLogOrig = guardLoc;
 
             var metObstacles = new List<GuardPointDir>();
-            var retconSpots = new List<Point>();
 
             AdvancementResult result;
 
             do
             {
-                result = AdvanceGuard(
-                    map, metObstacles, ref guardLoc, retconSpots);
+                result = AdvanceGuard(map, metObstacles, ref guardLoc);
             } while (result == AdvancementResult.InMap);
 
             if (result == AdvancementResult.Revisit)
@@ -196,9 +190,37 @@ namespace AoC2024Unified.Solutions
 
             int totalVisited = CountVisited(map);
 
+            var retconSpots = new List<Point>();
+
+            TimeSpan bTime;
+            DateTime start = DateTime.Now;
+
+            foreach (string[] testMap in AddOneObstacle(mapOrig))
+            {
+                AdvancementResult retconResult;
+                Point testGuardLoc = guardLogOrig;
+                var testMetObstacles = new List<GuardPointDir>();
+
+                do
+                {
+                    retconResult = AdvanceGuard(
+                        testMap, testMetObstacles, ref testGuardLoc);
+                } while (retconResult == AdvancementResult.InMap);
+
+
+                if (retconResult == AdvancementResult.Revisit)
+                {
+                    retconSpots.Add(guardLoc);
+                }
+            }
+
+            DateTime end = DateTime.Now;
+            bTime = end - start;
+
             Console.WriteLine($"The guard has visited {totalVisited} spots");
             Console.WriteLine(
-                $"Possible retcon spots: {retconSpots.Count}");
+                $"Possible retcon spots: {retconSpots.Count}."
+                + $"(Took {bTime.TotalSeconds} seconds)");
         }
     }
 }
