6d7aa589bb4ea141fb9da6c9ed2f55bb3cafd200
Rea Klairi Tasopoulou <104150585+GistOfSpirit@users.noreply.github.com>
2024-12-08 14:56:48 +0000

    Added solution for 8B

--- AoC2024Unified/AoC2024Unified/Solutions/Day8Solution.cs
+++ AoC2024Unified/AoC2024Unified/Solutions/Day8Solution.cs
@@ -7,7 +7,7 @@ namespace AoC2024Unified.Solutions
     {
         private const int DayNum = 8;
 
-        private static int CountAntinodes(
+        private static int CountAntinodesA(
             List<MarkedPoint> antennae, Rectangle matrixSize)
         {
             var antinodes = new List<Point>();
@@ -27,25 +27,89 @@ namespace AoC2024Unified.Solutions
                 {
                     MarkedPoint relAnt1 = relevantAntennae[x];
 
-                    for (int y = x; y < relevantAntennae.Count; ++y)
+                    for (int y = x + 1; y < relevantAntennae.Count; ++y)
                     {
                         MarkedPoint relAnt2 = relevantAntennae[y];
 
-                        if (relAnt1 != relAnt2)
+                        Size distance = relAnt1 - relAnt2;
+                        Point an1 = relAnt1 + distance;
+                        Point an2 = relAnt2 - distance;
+
+                        if (matrixSize.Contains(an1))
                         {
-                            Size distance = relAnt1 - relAnt2;
-                            Point an1 = relAnt1 + distance;
-                            Point an2 = relAnt2 - distance;
+                            antinodes.Add(an1);
+                        }
 
-                            if (matrixSize.Contains(an1))
-                            {
-                                antinodes.Add(an1);
-                            }
+                        if (matrixSize.Contains(an2))
+                        {
+                            antinodes.Add(an2);
+                        }
+                    }
+                }
+            }
 
-                            if (matrixSize.Contains(an2))
-                            {
-                                antinodes.Add(an2);
-                            }
+            return antinodes.Distinct().Count();
+        }
+
+        private static int GCD(int a, int b)
+        {
+            // "Stolen" code
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a | b;
+        }
+
+        private static int CountAntinodesB(
+            List<MarkedPoint> antennae, Rectangle matrixSize)
+        {
+            var antinodes = new List<Point>();
+
+            List<char> antennaTypes = antennae
+                .Select((a) => a.Mark)
+                .Distinct()
+                .ToList();
+
+            foreach (char antennaType in antennaTypes)
+            {
+                List<MarkedPoint> relevantAntennae = antennae
+                    .Where((a) => a.Mark == antennaType)
+                    .ToList();
+
+                for (int x = 0; x < relevantAntennae.Count; ++x)
+                {
+                    MarkedPoint relAnt1 = relevantAntennae[x];
+
+                    for (int y = x + 1; y < relevantAntennae.Count; ++y)
+                    {
+                        MarkedPoint relAnt2 = relevantAntennae[y];
+
+                        Size distance = relAnt1 - relAnt2;
+                        int minDistCoord = GCD(
+                            Math.Abs(distance.Width),
+                            Math.Abs(distance.Height));
+                        Size step = distance / minDistCoord;
+
+                        for (Point i = relAnt1.AsPoint();
+                            matrixSize.Contains(i);
+                            i += step
+                        )
+                        {
+                            antinodes.Add(i);
+                        }
+
+                        for (
+                            Point i = relAnt1.AsPoint() - step;
+                            matrixSize.Contains(i);
+                            i -= step
+                        )
+                        {
+                            antinodes.Add(i);
                         }
                     }
                 }
@@ -61,10 +125,17 @@ namespace AoC2024Unified.Solutions
             List<MarkedPoint> antennae = MarkedPoint.ParseMatrix(
                 matrix, char.IsLetterOrDigit);
 
-            int antinodeCount = CountAntinodes(antennae,
-                new Rectangle(0, 0, matrix[0].Length, matrix.Length));
+            var matrixSize = new Rectangle(
+                0, 0, matrix[0].Length, matrix.Length);
+
+            int antinodeCount = CountAntinodesA(antennae, matrixSize);
 
             Console.WriteLine($"Found {antinodeCount} antinodes.");
+
+            int harmonicAnCount = CountAntinodesB(antennae, matrixSize);
+
+            Console.WriteLine(
+                $"Found {harmonicAnCount} antinodes with harmonics.");
         }
     }
 }
