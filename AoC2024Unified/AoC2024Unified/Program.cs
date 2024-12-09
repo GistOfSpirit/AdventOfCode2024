91784148c32bf348727da9818fbb1cc2af719862
Rea Klairi Tasopoulou <104150585+GistOfSpirit@users.noreply.github.com>
2024-12-09 14:11:01 +0000

    Refactored Program.cs

--- AoC2024Unified/AoC2024Unified/Program.cs
+++ AoC2024Unified/AoC2024Unified/Program.cs
@@ -1,18 +1,21 @@
-﻿using System.Drawing;
-using AoC2024Unified.Solutions;
+﻿using AoC2024Unified.Solutions;
 
-var daySolutions = new IDaySolution[]{
-    new Day1Solution(),
-    new Day2Solution(),
-    new Day3Solution(),
-    new Day4Solution(),
-    new Day5Solution(),
-    new Day6Solution(),
-    new Day7Solution(),
-    new Day8Solution()
+var daySolutions = new Type[]{
+    typeof(Day1Solution),
+    typeof(Day2Solution),
+    typeof(Day3Solution),
+    typeof(Day4Solution),
+    typeof(Day5Solution),
+    typeof(Day6Solution),
+    typeof(Day7Solution),
+    typeof(Day8Solution),
+    typeof(Day9Solution),
 };
 
 int dayNum = 8;
 bool isReal = false;
 
-await daySolutions[dayNum - 1].Solve(isReal);
+var daySolution = (IDaySolution)Activator.CreateInstance(
+    daySolutions[dayNum - 1])!;
+
+await daySolution.Solve(isReal);
