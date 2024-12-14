b49d01bedec6e0d288a7868f9cbbba80f37efd41
Rea Klairi Tasopoulou <104150585+GistOfSpirit@users.noreply.github.com>
2024-12-14 21:51:44 +0000

    Refactored Program.cs

--- AoC2024Unified/AoC2024Unified/Program.cs
+++ AoC2024Unified/AoC2024Unified/Program.cs
@@ -1,27 +1,18 @@
 ﻿using AoC2024Unified.Solutions;
 
-var daySolutions = new Type[]{
-    typeof(Day1Solution),
-    typeof(Day2Solution),
-    typeof(Day3Solution),
-    typeof(Day4Solution),
-    typeof(Day5Solution),
-    typeof(Day6Solution),
-    typeof(Day7Solution),
-    typeof(Day8Solution),
-    typeof(Day9Solution),
-    typeof(Day10Solution),
-    typeof(Day11Solution),
-    typeof(Day12Solution),
-    typeof(Day13Solution),
-    typeof(Day14Solution)
-};
-
 int dayNum = 14;
 bool isReal = false;
 
-var daySolution = (IDaySolution)Activator.CreateInstance(
-    daySolutions[dayNum - 1])!;
+Type? daySolutionType = Type.GetType(
+    $"AoC2024Unified.Solutions.Day{dayNum}Solution");
+
+if (daySolutionType == null)
+{
+    Console.WriteLine($"Could not find solution for day {dayNum}");
+    return;
+}
+
+var daySolution = (IDaySolution)Activator.CreateInstance(daySolutionType)!;
 
 if (
     args.Length > 0
